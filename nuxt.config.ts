65f684d10bd71b092ec668db5131b2fa9203bccb
Marat Timofeev <marat.tmv1@gmail.com>
2024-09-08 13:17:22 +0700

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,45 @@
+import vueCssModule from "@inetss/vite-plugin-vue-css-module"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  compatibilityDate: "2024-08-09",
+
+  modules: ["@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
+
+  ssr: false,
+
+  nitro: {
+    esbuild: {
+      options: {
+        // For native bigints.
+        target: "ESNext",
+      },
+    },
+  },
+
+  vite: {
+    plugins: [
+      vueCssModule({ attrName: "mclass", pugClassLiterals: true }),
+    ],
+  },
+
+  css: [
+    "~/styles/index.scss",
+  ],
+
+  piniaPersistedstate: {
+    storage: "localStorage",
+  },
+
+  runtimeConfig: {
+    // Token for accessing admin API
+    adminToken: "",
+    baseUrl: "",
+    database: {
+      url: "postgres://localhost/fugu",
+      log: false,
+    },
+  },
 })
