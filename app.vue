1a204ac2b77472f7ef551123f894c9e522c49713
Marat Timofeev <marat.tmv1@gmail.com>
2024-09-08 14:10:38 +0700

    add fugu image

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+<script setup lang="ts">
+</script>
+
+<template lang="pug">
+div
+  img(src="/assets/cute_fugu.png")
 </template>
