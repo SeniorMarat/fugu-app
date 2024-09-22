9a7ef9ffedfe931ca858257647d9424d29ddd0f4
Marat Timofeev <marat.tmv1@gmail.com>
2024-09-22 17:51:51 +0700

    Сделал username computed

--- components/score.vue
+++ components/score.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const { useWebApp } = await import("vue-tg")
-const { initDataUnsafe } = await useWebApp()
-const username = initDataUnsafe.user?.username.toUpperCase() || "SUNDVR"
+const { initDataUnsafe } = useWebApp()
+const username = computed(() => initDataUnsafe.user?.username.toUpperCase() || "SUNDVR")
 </script>
 
 <template lang="pug">
