5a57e8cbb178638ab179d86249f1e789b390b652
Marat Timofeev <marat.tmv1@gmail.com>
2024-09-23 16:01:36 +0700

    Добавил кнопки магазин и кошелёк

--- components/nav-button.vue
+++ components/nav-button.vue
@@ -5,36 +5,64 @@ const props = defineProps<{
   label: string
   icon: string
   path?: string
+  rounded?: "all" | "left" | "right"
 }>()
 
 function get_svg_path(name: string) {
   return new URL(`/public/${name}.svg`, import.meta.url).href
 }
+
+const roundedClass = computed(() => {
+  if (props.rounded === "all") {
+    return "all-rounded"
+  }
+  if (props.rounded === "left") {
+    return "left-rounded"
+  }
+  if (props.rounded === "right") {
+    return "right-rounded"
+  }
+  return "all-rounded"
+})
 </script>
 
 <template lang="pug">
-o-button.nav-button(tag="router-link" :to="props.path")
-  img(class="icon" :src="get_svg_path(props.icon)")
+o-button.nav-button.all_rounded(:class="roundedClass" tag="router-link" :to="props.path")
+  img(class="icon" :src="get_svg_path(props.icon)" style="width: 50%;")
   | {{ props.label }}
 </template>
 
-<style module lang="scss">
+<style lang="scss">
 .nav-button {
   width: 61px;
   height: 61px;
   display: flex;
   flex-direction: column;
-  justify-content: flex-end;
   align-items: center;
+  justify-content: center;
   background-color: #3c71a23b;
+  text-align: center;
   color: #fff;
   border: 2px solid #c3cfd933;
   box-shadow: 4px 4px 4px 0px #00000040;
-  border-radius: 20px;
   font-family: "Inter";
   font-size: 16px;
   font-weight: 700;
   text-decoration: none;
+
+  &.all-rounded {
+    border-radius: 20px;
+  }
+
+  &.right-rounded {
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+  }
+
+  &.left-rounded {
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
 }
 
 .nav-button:focus {
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,9 @@ import { OButton } from "@oruga-ui/oruga-next"
 <template lang="pug">
 .page
   score
+  div(style="display: flex; flex-direction: row; align-items: center; justify-content: space-between; width: 100%;")
+    nav-button(icon="shop" label="shop" rounded="right" path="/shop")
+    nav-button(icon="wallet" label="wallet" rounded="left" path="/wallet")
   div(@click="() => $router.push('/game')").fugu
     img(src="/public/small_fugu.png" style="width: 100%;")
   o-button.feed Feed
@@ -52,5 +55,32 @@ import { OButton } from "@oruga-ui/oruga-next"
   height: 60px;
   box-shadow: 0px 4px 4px 0px #00000040;
   border: 0.5px solid #000;
+}
+
+.side-button {
+  width: 61px;
+  height: 61px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: #3c71a23b;
+  color: #fff;
+  border: 2px solid #c3cfd933;
+  box-shadow: 4px 4px 4px 0px #00000040;
+  font-family: "Inter";
+  font-size: 16px;
+  font-weight: 700;
+  text-decoration: none;
+}
+
+.side-button.left {
+  border-top-right-radius: 15px;
+  border-bottom-right-radius: 15px;
+}
+
+.side-button.right {
+  border-top-left-radius: 15px;
+  border-bottom-left-radius: 15px;
 }
 </style>
