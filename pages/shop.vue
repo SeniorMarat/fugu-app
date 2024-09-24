97397fa810a7e315912059f776d0ec47383c5a93
Marat Timofeev <marat.tmv1@gmail.com>
2024-09-24 17:05:21 +0700

    Добавил заглушку для страницы магазина

---
+++ pages/shop.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { OButton } from "@oruga-ui/oruga-next"
+import { useTonConnectUI } from "@townsquarelabs/ui-vue"
+
+const [TonConnectUI] = useTonConnectUI()
+
+const my_transaction = {
+  validUntil: Math.floor(Date.now() / 1000) + 60,
+  messages: [
+    {
+      address: "UQDhTbUFoREWU968r97w_nwvrcR991bqeVXLYX_TO6UcgRzS",
+      amount: "1",
+    },
+    {
+      address: "UQDhTbUFoREWU968r97w_nwvrcR991bqeVXLYX_TO6UcgRzS",
+      amount: "2",
+    },
+  ],
+}
+
+function send_transaction() {
+  TonConnectUI.sendTransaction(my_transaction)
+}
+</script>
+
+<template lang="pug">
+  o-button(@click="send_transaction") send transaction
+</template>
