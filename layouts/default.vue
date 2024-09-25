5ad61682aaeeeb60baec5f147cb7a827adc9392b
Marat Timofeev <marat.tmv1@gmail.com>
2024-09-25 17:29:48 +0700

    Добавил анимацию для фона

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,9 +11,11 @@
 
 <style module lang="scss">
 body {
-  background-image: url("/public/background.jpg");
+  background: linear-gradient(286deg, rgba(72, 134, 246, 1) 3%, rgba(15, 185, 172, 1) 51%, rgba(0, 212, 255, 1) 100%);
   background-size: cover;
   overflow: hidden;
+  animation: gradient 10s infinite linear;
+  background-size: 400%;
 }
 .layout {
   display: flex;
@@ -32,5 +34,17 @@ body {
 .page {
   flex: 1;
   overflow-y: auto;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 80% 0%;
+  }
+  50% {
+    background-position: 20% 100%;
+  }
+  100% {
+    background-position: 80% 0%;
+  }
 }
 </style>
