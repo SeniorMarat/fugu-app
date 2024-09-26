536fe14542234e13282ce5a94c221e18b7dd4441
Marat Timofeev <marat.tmv1@gmail.com>
2024-09-26 21:59:46 +0700

    Поменял градиент

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,41 +3,6 @@
 
 <template lang="pug">
 .layout
-  #bg-wrap
-  svg(viewBox="0 0 100 100" preserveAspectRatio="xMidYMid slice")
-    defs
-      radialGradient#Gradient1(cx="50%" cy="50%" fx="0.441602%" fy="50%" r=".5")
-        animate(attributeName="fx" dur="34s" values="0%;3%;0%" repeatCount="indefinite")
-        stop(offset="0%" stop-color="rgba(255, 0, 255, 1)")
-        stop(offset="100%" stop-color="rgba(255, 0, 255, 0)")
-      radialGradient#Gradient2(cx="50%" cy="50%" fx="2.68147%" fy="50%" r=".5")
-        animate(attributeName="fx" dur="23.5s" values="0%;3%;0%" repeatCount="indefinite")
-        stop(offset="0%" stop-color="rgba(255, 255, 0, 1)")
-        stop(offset="100%" stop-color="rgba(255, 255, 0, 0)")
-      radialGradient#Gradient3(cx="50%" cy="50%" fx="0.836536%" fy="50%" r=".5")
-        animate(attributeName="fx" dur="21.5s" values="0%;3%;0%" repeatCount="indefinite")
-        stop(offset="0%" stop-color="rgba(0, 255, 255, 1)")
-        stop(offset="100%" stop-color="rgba(0, 255, 255, 0)")
-      radialGradient#Gradient4(cx="50%" cy="50%" fx="4.56417%" fy="50%" r=".5")
-        animate(attributeName="fx" dur="23s" values="0%;5%;0%" repeatCount="indefinite")
-        stop(offset="0%" stop-color="rgba(0, 255, 0, 1)")
-        stop(offset="100%" stop-color="rgba(0, 255, 0, 0)")
-      radialGradient#Gradient5(cx="50%" cy="50%" fx="2.65405%" fy="50%" r=".5")
-        animate(attributeName="fx" dur="24.5s" values="0%;5%;0%" repeatCount="indefinite")
-        stop(offset="0%" stop-color="rgba(0,0,255, 1)")
-        stop(offset="100%" stop-color="rgba(0,0,255, 0)")
-      radialGradient#Gradient6(cx="50%" cy="50%" fx="0.981338%" fy="50%" r=".5")
-        animate(attributeName="fx" dur="25.5s" values="0%;5%;0%" repeatCount="indefinite")
-        stop(offset="0%" stop-color="rgba(255,0,0, 1)")
-        stop(offset="100%" stop-color="rgba(255,0,0, 0)")
-    rect(x="13.744%" y="1.18473%" width="100%" height="100%" fill="url(#Gradient1)" transform="rotate(334.41 50 50)")
-      animate(attributeName="x" dur="20s" values="25%;0%;25%" repeatCount="indefinite")
-      animate(attributeName="y" dur="21s" values="0%;25%;0%" repeatCount="indefinite")
-      animateTransform(attributeName="transform" type="rotate" from="0 50 50" to="360 50 50" dur="7s" repeatCount="indefinite")
-    rect(x="-2.17916%" y="35.4267%" width="100%" height="100%" fill="url(#Gradient2)" transform="rotate(255.072 50 50)")
-      animate(attributeName="x" dur="23s" values="-25%;0%;-25%" repeatCount="indefinite")
-      animate(attributeName="y" dur="24s" values="0%;50%;0%" repeatCount="indefinite")
-      animateTransform(attributeName="transform" type="rotate" from="360 50 50" to="0 50 50" dur="9s" repeatCount="indefinite")
   bubbles
   .main
     .page: nuxt-page
@@ -46,7 +11,16 @@
 
 <style module lang="scss">
 body {
-  background-color: aqua;
+  background: linear-gradient(
+    149deg,
+    rgba(0, 255, 255, 1) 0%,
+    rgba(0, 191, 255, 1) 42%,
+    rgba(187, 24, 148, 1) 72%,
+    rgba(65, 105, 225, 1) 100%
+  );
+  animation: gradient 10s infinite linear;
+  background-size: 400%;
+
   background-size: cover;
   overflow: hidden;
   background-size: 400%;
