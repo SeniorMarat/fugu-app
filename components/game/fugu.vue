92acc0217b70270532db7eeaa4bb393008d94d75
Marat Timofeev <marat.tmv1@gmail.com>
2024-10-09 17:39:53 +0700

    Добавил надувание рыбы

--- components/game/fugu.vue
+++ components/game/fugu.vue
@@ -5,6 +5,7 @@ Component for handling bird ans it's position
 <script setup lang="ts">
 const x = ref(100) // initial x position
 const y = ref(0) // initial y position
+const size = ref(25)
 const velocity = ref(0) // initial velocity
 const gravity = ref(0.3) // gravity constant
 const is_held = ref(false)
@@ -14,10 +15,18 @@ onMounted(() => {
   setInterval(() => {
     update()
   }, 16)
-  document.addEventListener("touchstart", () => is_held.value = true)
-  document.addEventListener("mousedown", () => is_held.value = true)
-  document.addEventListener("mouseup", () => is_held.value = false)
-  document.addEventListener("touchend", () => is_held.value = false)
+  document.addEventListener("touchstart", () => {
+    is_held.value = true
+  })
+  document.addEventListener("mousedown", () => {
+    is_held.value = true
+  })
+  document.addEventListener("mouseup", () => {
+    is_held.value = false
+  })
+  document.addEventListener("touchend", () => {
+    is_held.value = false
+  })
 })
 
 function update() {
@@ -38,11 +47,30 @@ function update() {
     y.value = document.documentElement.clientHeight - 51
     velocity.value = 0
   }
+  if (is_held.value) {
+    size.value += 10
+    x.value -= 5
+    y.value -= 5
+  } else {
+    size.value -= 10
+    x.value += 5
+    y.value += 5
+  }
+  if (size.value > 100) {
+    size.value = 100
+    x.value += 5
+    y.value += 5
+  }
+  if (size.value < 50) {
+    size.value = 50
+    x.value -= 5
+    y.value -= 5
+  }
 }
 </script>
 
 <template lang="pug">
-.fugu(:style="{ left: `${x}px`, top: `${y}px` }")
+.fugu(:style="{ left: `${x}px`, top: `${y}px`, width: `${size}px`, height: `${size}px` }")
   // Add your pufferfish graphics here
   img(src="/public/angry-fugu.png" style="width: 100%; transform: scaleX(-1);")
 </template>
@@ -50,7 +78,5 @@ function update() {
 <style module lang="scss">
 .fugu {
   position: absolute;
-  width: 50px; /* adjust to match your pufferfish graphics */
-  height: 50px; /* adjust to match your pufferfish graphics */
 }
 </style>
