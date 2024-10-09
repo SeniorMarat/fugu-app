148104c5ac035d7aacc5a3ac7bb0d7a7d8dd6f8c
Marat Timofeev <marat.tmv1@gmail.com>
2024-10-09 17:27:21 +0700

    Исправил игру

--- layouts/game.vue
+++ layouts/game.vue
@@ -3,7 +3,14 @@ import { OButton } from "@oruga-ui/oruga-next"
 </script>
 
 <template lang="pug">
-  .layout
-  o-button(@click="() => $router.push('/')") Back
-    .page: nuxt-page
+.layout
+  o-button(style="position: absolute" @click="() => $router.push('/')") Back
+  .page: nuxt-page
 </template>
+
+<style module lang="scss">
+body {
+  background: aqua;
+  overflow: hidden;
+}
+</style>
