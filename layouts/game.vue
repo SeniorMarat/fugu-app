53726fe176734c4e151f05ff004cf5ea6004e1ab
Marat Timofeev <marat.tmv1@gmail.com>
2024-10-26 22:00:22 +0700

    Добавил рыбок на фон

--- layouts/game.vue
+++ layouts/game.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { OButton } from "@oruga-ui/oruga-next"
+const fishes = [{ x: 11, y: 13 }, { x: 15, y: 20 }, { x: 17, y: 10 }, { x: 60, y: 10 }, { x: 70, y: 13 }]
+const fishes_plavnik = [{ x: 51, y: 53 }, { x: 55, y: 58 }, { x: 52, y: 60 }, { x: 10, y: 90 }, { x: 15, y: 95 }]
 </script>
 
 <template lang="pug">
 .layout
+  img.fish(v-for="(fish, index) in fishes" :key="index" src="/public/obstacles/fish.svg" :style="{ left: `${fish.x}%`, top: `${fish.y}%` }")
+  img.fish(v-for="(fish, index) in fishes_plavnik" :key="index" src="/public/obstacles/fish-plavnik.svg" :style="{ left: `${fish.x}%`, top: `${fish.y}%` }")
   .main
     .page: nuxt-page
 </template>
@@ -37,5 +40,10 @@ body {
 }
 .page {
   flex: 1;
+}
+.fish {
+  position: absolute;
+  height: 2vh;
+  opacity: 0.1;
 }
 </style>
