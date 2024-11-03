55029fd4d84b7930c624866e100e7c67c2fcf500
Marat Timofeev <marat.tmv1@gmail.com>
2024-11-03 15:10:05 +0700

    Поменял стены и монетку

--- components/game/walls.vue
+++ components/game/walls.vue
@@ -8,7 +8,7 @@ const { height } = useWindowSize()
 const wall_speed = ref(2.5)
 const wall_height = computed(() => height.value / 2.0)
 
-const walls = ref<Array<Ref<{ y: number }>>>([ref({ y: wall_height.value }), ref({ y: +5 }), ref({ y: -wall_height.value + 10 })])
+const walls = ref<Array<Ref<{ y: number, img: string }>>>([ref({ y: wall_height.value, img: "wall1.svg" }), ref({ y: +5, img: "wall2.svg" }), ref({ y: -wall_height.value + 10, img: "wall3.svg" })])
 
 function get_wall_path(name: string) {
   return new URL(`/public/obstacles/${name}`, import.meta.url).href
@@ -23,7 +23,9 @@ onMounted(() => {
         }
       }
       if (walls.value!.at(-1)!.value.y >= -5) {
-        walls.value.push(ref({ y: -wall_height.value }))
+        const names = ["wall1.svg", "wall2.svg", "wall3.svg"]
+        const random_index = Math.floor(Math.random() * names.length)
+        walls.value.push(ref({ y: -wall_height.value, img: names[random_index] }))
       }
     }
   }, 16)
@@ -33,8 +35,8 @@ onMounted(() => {
 <template lang="pug">
 .walls-container(:style="{ height: `${height}px` }")
   div(v-for="(wall, index) in walls" :key="index")
-    img(:src="get_wall_path('wall.svg')" :style="{ height: `${wall_height}px`, left: '-6.4vh', top: `${wall.value.y}px`, position: 'absolute' }")
-    img(:src="get_wall_path('wall.svg')" :style="{ height: `${wall_height}px`, right: '-6.4vh', top: `${wall.value.y}px`, position: 'absolute' }")
+    img(:src="get_wall_path(wall.value.img)" :style="{ height: `${wall_height}px`, left: '-6.4vh', top: `${wall.value.y}px`, position: 'absolute' }")
+    img(:src="get_wall_path(wall.value.img)" :style="{ height: `${wall_height}px`, right: '-6.4vh', top: `${wall.value.y}px`, position: 'absolute' }")
 </template>
 
 <style module lang="scss">
